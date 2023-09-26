a440289c1d50c0470ae855062b69d4b3fd909f52
Vibin Daniel <10978639+vibindaniel@users.noreply.github.com>
2023-09-26 08:37:46 -0400

    [CLOUD-2314] bump and fix lints

---
+++ examples/default/providers.tf
@@ -0,0 +1,5 @@
+provider "azuread" {}
+
+provider "aws" {}
+
+provider "random" {}
--- main.tf
+++ main.tf
@@ -261,7 +261,10 @@ resource "azuread_app_role_assignment" "main_app" {
   resource_object_id  = azuread_service_principal.main_app.object_id
 }
 
+#tfsec:ignore:aws-ssm-secret-use-customer-key
 resource "aws_secretsmanager_secret" "app_secrets" {
+  #checkov:skip=CKV_AWS_149:Skip using KMS CMR
+  #checkov:skip=CKV2_AWS_57:Skip auto-rotation
   name                           = "azure-app-${local.kebab_name}"
   recovery_window_in_days        = 0
   force_overwrite_replica_secret = true
@@ -274,8 +277,8 @@ resource "aws_secretsmanager_secret_version" "app_secret_version" {
     "AUTH__IssuerUrl"     = "https://login.microsoftonline.com/${data.azuread_client_config.current.tenant_id}/v2.0"
     "AUTH__ClientId"      = azuread_application.main_app.application_id
     "AUTH__ClientSecret"  = azuread_application_password.main_app.value
-    "AUTH__TenantId"         = data.azuread_client_config.current.tenant_id
-    "AUTH__Scopes"         = join(" ", [for k, v in local.ms_graph_scopes : k])
+    "AUTH__TenantId"      = data.azuread_client_config.current.tenant_id
+    "AUTH__Scopes"        = join(" ", [for k, v in local.ms_graph_scopes : k])
     "AUTH__TokenEndpoint" = "https://login.microsoftonline.com/${data.azuread_client_config.current.tenant_id}/oauth2/token"
   })
 }
